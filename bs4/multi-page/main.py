97823df6875dcc153ad2142401500c17b62dfb77
Fadilah Riczky <friczky@gmail.com>
2024-11-23 18:23:19 +0700

    multi page scraping data

--- bs4/multi-page/main.py
+++ bs4/multi-page/main.py
@@ -1,19 +1,61 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import time
 
 root = 'https://subslikescript.com'
-website = f'{root}/movies'
+website = f'{root}/movies_letter-A'
 result = requests.get(website)
 content = result.text
-
 soup = BeautifulSoup(content, 'lxml')
 # print(soup.prettify())
 
-box = soup.find('article', class_='main-article')
-links = box.findAll('a', href=True)
+# pagination
+pagination = soup.find('ul', class_='pagination')
+pages = pagination.find_all('li', class_='page-item')
+last_page = pages[-2].text
+print("Last page is: ", last_page)
 
-links_list = []
-for link in links:
-    links_list.append(root + link['href'])
 
-print(links_list)
+for page in range(1, int(last_page)+1):
+    print(f"Scraping page {page}")
+    print(f'{website}?page={page}')
+    result = requests.get(f'{website}?page={page}')
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    box = soup.find('article', class_='main-article')
+
+    links = []
+    for link in box.find_all('a', href=True):
+        links.append(link['href'])
+
+    # print(links)
+
+    for link in links:
+        time.sleep(2)
+        try:
+            result = requests.get(f'{root}/{link}')
+            content = result.text
+            soup = BeautifulSoup(content, 'lxml')
+
+            box = soup.find('article', class_='main-article')
+
+            title = box.find('h1').get_text()
+            transcript = box.find('div', class_='full-script').get_text(strip=True , separator='\n')
+
+            print("============== SCRAPED ==============")
+            print("   title :", title)
+            print("   time :" , time.strftime("%H:%M:%S", time.localtime()))
+            print("   file_name :", title+".txt")
+            print("=====================================")
+            os.makedirs('movies', exist_ok=True)
+            with open(os.path.join('movies', f'{title}.txt'), 'w', encoding='utf-8') as file:
+                    file.write(transcript)
+        except:
+            print("____Link not working____")
+            print(link)
+
+
+
+print("Scraping done!")
